bfd1c5c7708468bc8407ce18baeb237986bf6026
Apoorv Shah <apoorv.shah10@thg.com>
2024-11-20 11:50:45 +0530

    add pikachu pet

---
+++ lua/pets/pets/pikachu.lua
@@ -0,0 +1,25 @@
+return {
+next_actions = {
+        crouch = { "idle" },
+        idle = { "idle" },
+        idle_blink = { "idle" },
+        liedown = { "idle" },
+        sit = { "idle" },
+        sneak = { "idle" },
+        walk = { "idle" },
+    },
+    idle_actions = { "idle" },
+    first_action = "idle",
+    movements = {
+        right = {
+            normal = { "idle" },
+            fast = { "idle" },
+            slow = { "idle" },
+        },
+        left = {
+            normal = { "idle" },
+            fast = { "idle" },
+            slow = { "idle" },
+        },
+    },
+}
